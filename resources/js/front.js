f9855e25b3d7c648262bf23802ede0e432fe87a3
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-07-29 15:09:40 +0200

    installato Vue Router 3

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,12 +1,13 @@
 require('./bootstrap');
 
-window.Vue = require('vue');
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+// window.Vue = require('vue');
+import Vue from 'vue';   // questocodice è uguale a window.Vue = require('vue');
+import VueRouter from 'vue-router';
 import App from './components/App.vue';
 
+Vue.use(VueRouter)   // dentro alla parentesi ci vuole il nome indicato in riga 5
+
 const app = new Vue({
-    el: '#root',
+    el: '#root',          // id del componente nel file HTML dentro il quale operera Vue
     render: h => h(App),  // significato di render: in questa root monta il componente che si chiama App
 });
